67cdc15c9dad3c466426a3fbd5d8bf915db0c582
tzschiet <7867845+tzschiet@users.noreply.github.com>
2021-01-11 18:56:54 +0100

    Add minor refactorings and change application.properties to .yml

--- src/main/kotlin/de/leipzigtech/ba/BaApplication.kt
+++ src/main/kotlin/de/leipzigtech/ba/BaApplication.kt
@@ -3,7 +3,7 @@ package de.leipzigtech.ba
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
 
 
 @SpringBootApplication
